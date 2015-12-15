541d5ce041f8225486df5507fa68c5de4161a2b8
Jennifer Taggart <jennifer.taggart.work@gmail.com>
2015-12-15 11:26:33 -0800

    Experimental branch: I am the greatest

--- script.py
+++ script.py
@@ -1,4 +1,7 @@
 # All rights reserved
+#I am the greatest
+#This is the best script ever
+#Everything is awesome
 # Analyze bird counts
 
 Read data file
@@ -10,3 +13,7 @@ Save table
 Make header bold
 
 Save small figure, red line
+
+Make another figure
+
+Make another figure
